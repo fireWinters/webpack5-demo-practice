d603b60de0bed42d5025574afa92f33c5a68f99c
唐王瑶 <tangwangyao@hualala.com>
2023-02-14 16:58:46 +0800

    feat: use config file,create bundle

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: 唐王瑶
+ * @Email：tangwangyao@hualala.com
+ * @Date: 2023-02-14 14:56:18
+ * @LastEditors: 唐王瑶
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /webpack5-demo-practice/src/index.js
+ */
+import _ from 'lodash';
+function component() {
+  const element = document.createElement('div');
+  // lodash 在当前 script 中使用 import 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+}
+document.body.appendChild(component());
