4fb0700675a52cfd0aa16992b4ee4cc6dddc6be1
唐王瑶 <tangwangyao@hualala.com>
2023-02-14 17:30:57 +0800

    feat: add css config

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@
  * @FilePath: /webpack5-demo-practice/src/index.js
  */
 import _ from 'lodash';
+
+import './style.css';
 function component() {
   const element = document.createElement('div');
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
   return element;
 }
 document.body.appendChild(component());
