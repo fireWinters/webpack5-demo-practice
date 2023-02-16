f9a31c7580e1a1b956bd8a89a3d81eea84da7ea7
唐王瑶 <tangwangyao@hualala.com>
2023-02-16 16:11:14 +0800

    feat: change output config

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@
  */
 import _ from 'lodash';
 
+import printMe from './print';
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 document.body.appendChild(component());
