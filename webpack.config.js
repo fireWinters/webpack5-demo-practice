7b4217305569db84ff359e1abdd59b21850cef55
唐王瑶 <tangwangyao@hualala.com>
2023-02-16 10:05:55 +0800

    feat: add font file and config it

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ rules:[
     test:/\.(png|svg|jpg|jpeg|gif|tif|tiff)$/i,
     type:'asset/resource',
     },
+    {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
 ]
 },
 }
