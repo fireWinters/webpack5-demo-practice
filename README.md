ad288dfc2e0a750a054672403f14bbdc5f18300d
唐王瑶 <tangwangyao@hualala.com>
2023-02-16 15:33:36 +0800

    feat: add config about toml、yaml or json5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webpack5-demo-practice
 练习使用webpack5的API
+自定义 JSON 模块 parser
+首先安装 toml，yamljs 和 json5
+在webpack 中配置它们
--- src/index.js
+++ src/index.js
@@ -8,8 +8,23 @@
  */
 import _ from 'lodash';
 
+import Data from './data.xml';
+import Notes from './data.csv';
 import './style.css';
 import  Icon from './diana.jpeg';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 function component() {
   const element = document.createElement('div');
   // lodash 在当前 script 中使用 import 引入
@@ -18,6 +33,8 @@ function component() {
   const imgs = new Image();
   imgs.src = Icon;
   element.appendChild(imgs);
+  console.log(Data);
+  console.log(Notes);
   return element;
 }
 document.body.appendChild(component());
