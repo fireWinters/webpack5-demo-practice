1ea4a1ed18331b901dd662db073fc2f0c56e1a9a
唐王瑶 <tangwangyao@hualala.com>
2023-02-17 14:26:51 +0800

    feat: feat HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
 entry:{
     index:'./src/index.js',
     print:'./src/print.js'
 },
+plugins:[
+    new HtmlWebpackPlugin({
+        title:'管理输出内容'
+    }),
+],
 output:{
 path:path.resolve(__dirname,'dist'),
 // filename:'bundle.js'
